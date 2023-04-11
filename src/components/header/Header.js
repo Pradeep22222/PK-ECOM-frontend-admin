c6ad1c66baa39eb3bc50b019b0eed30b382924d9
PK <pradeepdhital124@gmail.com>
2023-04-11 13:31:23 +1000

    email verification handled on the page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,10 +8,15 @@ export const Header = () => {
     <Navbar bg="info" variant="light" expand="md">
       <Container>
         <Navbar.Brand href="#home">PK-ECOM</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Link className="nav-link" to="/">Login</Link>
-            <Link className="nav-link" to="/register">Register</Link>
+            <Link className="nav-link" to="/">
+              Login
+            </Link>
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/admin-registration/EmailVerification.js
+++ src/pages/admin-registration/EmailVerification.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Header } from "../../components/header/Header";
 import { Footer } from "../../components/footer/Footer";
-import { Card, Container, Spinner } from "react-bootstrap";
+import { Alert, Card, Container, Spinner } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import { emailVerifyAdminUser } from "../../helpers/axiosHelper";
 
@@ -17,22 +17,29 @@ const EmailVerification = () => {
     // call axios to call the server
     (async () => {
       const result = await emailVerifyAdminUser(obj);
+      setResponse(result);
+      setIsPending(false);
     })();
   }, []);
   return (
     <div>
       <Header></Header>
       <Container className="page-main">
-        {isPending && (
-          <Card className="mt-5 p-2" style={{ width: "45rem" }}>
-            <Spinner
-              variant="primary"
-              animation="border"
-              className="m-auto p-3"
-            ></Spinner>
-            <h4>Email verification process begun, please wait......</h4>
-          </Card>
-        )}
+        <Card className="mt-5 p-2" style={{ width: "45rem" }}>
+          {isPending && (
+            <div>
+              <Spinner
+                variant="primary"
+                animation="border"
+                className="m-auto p-3"
+              ></Spinner>
+              <h4>Email verification process begun, please wait......</h4>
+            </div>
+          )}
+          {response.message && (
+            <Alert variant={response.status==="success"?"success":"danger"}>{response.message}</Alert>
+          )}
+        </Card>
       </Container>
       <Footer></Footer>
     </div>
